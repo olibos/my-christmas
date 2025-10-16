91620a8b4484c3b1d3ed4193c2108a67a22d5cb9
Olivier Bossaer <olivier.bossaer@gmail.com>
2025-10-16 17:16:41 +0200

    update base page url  😉

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from 'vite';
 export default defineConfig({
-    //base: 'https://cdn.example.com/assets/',
+    base: 'https://olibos.github.io/my-christmas/',
     build: {
     rollupOptions: {
       output: {
